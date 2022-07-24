105b17c8e1d913f7b5ad6ab746704b7b2d0316a2
Brandon Fowler <42071238+CalypsoDevelopment@users.noreply.github.com>
2022-07-24 16:16:26 -0700

    Nuxt Config Change

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,11 @@ export default {
     '@nuxtjs/axios',
     'nuxt-sweetalert2'
   ],
-
+  generate: {
+    target: 'static',
+    ssr: true,
+    fallback: true
+  },
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
