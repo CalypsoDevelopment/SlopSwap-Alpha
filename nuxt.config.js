e1c459da82501623fccdae22ae948213a6fd0ca2
Brandon Fowler <42071238+CalypsoDevelopment@users.noreply.github.com>
2022-07-24 18:00:04 -0700

    Raised the position of target static in Nuxt Config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,9 @@
 export default {
+  generate: {
+    target: 'static',
+    ssr: true,
+    fallback: true
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'slopswapx',
@@ -18,11 +23,6 @@ export default {
       { hid: 'fontawesome', src: 'https://kit.fontawesome.com/3b6ad1dd5e.js', crossorigin: 'anonymous' },
       { src: 'https://unpkg.com/qr-code-styling@1.5.0/lib/qr-code-styling.js' }
     ]
-  },
-  generate: {
-    target: 'static',
-    ssr: true,
-    fallback: true
   },
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
